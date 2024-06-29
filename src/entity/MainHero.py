34795a966381d09aabbe94a6366bddddd9497eb5
Nitor112221 <kostya1234moi@gmail,com>
2024-06-29 12:27:41 +0300

    сделана функция __str__ для класса главного героя, она выводит полную статистика по игроку

--- src/entity/MainHero.py
+++ src/entity/MainHero.py
@@ -2,12 +2,14 @@ from Base import Base
 
 
 class MainHero(Base):
-    def __init__(self, weapon, armor, lvl):
+    def __init__(self, weapon, armor, lvl, name):
         self.weapon = weapon
         self.armor = armor
         self.lvl = lvl
+        self.name = name
         self.inventory = []  # инвентарь хранит разные предметы, это могут быть еда для исцеления, броня и оружее
-        super().__init__(20 + (lvl - 1) * 3, weapon.get_damage + (lvl - 1) * 0.5, armor.get_protection + (lvl - 1) * 0.5)  # за каждый уровень добавляет + 3 хп и по 0.5 урона и брони
+        super().__init__(20 + (lvl - 1) * 3, weapon.get_damage + (lvl - 1) * 0.5, armor.get_protection + (
+                    lvl - 1) * 0.5)  # за каждый уровень добавляет + 3 хп и по 0.5 урона и брони
 
     def switch_weapon(self, new_weapon):
         self.inventory.remove(new_weapon)
@@ -23,3 +25,19 @@ class MainHero(Base):
 
     def use(self, index):  # индекс предмета в инвенторе
         self.inventory[index].use(self)  # у брони, оружия и еды есть метод use по принципу полиморфизм
+
+    def str_inventory(self):
+        return "\t\n".join(self.inventory)
+
+    def __str__(self):
+        return (
+            f"{self.name}:\n"
+            f"•Уровень: {self.lvl}\n"
+            f"•Максимальное здоровье: {self.max_hp}\n"
+            f"•Текущий уровень здоровья: {self.hp}"
+            f"•Защита: {self.protection}\n"
+            f"•Урон: {self.damage}\n"
+            f"•Оружее: {self.weapon}\n"
+            f"•Броня: {self.armor}\n"
+            f"•Инвентарь:\n"
+            f"    {self.str_inventory()}")
